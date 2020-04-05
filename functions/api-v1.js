a433c7ed5ff9c6553cd83bc352a55a016977ccfe
YuriiTrofimenko <tyaa@ukr.net>
2020-04-05 15:08:06 +0300

    api-v1 demo is done

--- functions/api-v1.js
+++ functions/api-v1.js
@@ -15,9 +15,42 @@ exports.aims = function (functions, admin) {
   // app.delete('/:id', (req, res) => res.send(Widgets.delete(req.params.id)));
   // app.get('/', (req, res) => res.send(Widgets.list()));
   app.get('/', (req, res) =>
-    admin.database().ref('AtEYfN8PrqdYI602FiZgrHL5SOp2' + '/nodes').once('value').then(async (data) => {
-      return res.send(data.val());
-    })
+    admin.database()
+      .ref('/')
+      .orderByChild('userdata/nodesUpdatedAt')
+      .limitToLast(10)
+      .once('value')
+      .then(async (data) => {
+        // return res.send(data.val());
+        const users = data.val()
+        const usersArray = []
+        if (users) {
+          // Get task key (id)
+          Object.keys(users).forEach(key => {
+            const u = users[key]
+            const nodes = u.nodes
+            const nodesArray = []
+            if (nodes) {
+              Object.keys(nodes).sort().reverse().forEach(key => {
+                const n = nodes[key]
+                // Если цель доступна и статус цели - не "отменено" и не "завершено"
+                if (n.access && n.status !== '5' && n.status !== '6') {
+                  nodesArray.push(
+                    {
+                      'title': n.title,
+                      'description': n.description,
+                      'createdAt': n.createdAt
+                    }
+                  )
+                }
+              })
+            }
+            const userEmail = (u.userdata) ? u.userdata.email : ('anonimous_' + (new Date()).toISOString())
+            usersArray.push({[userEmail]: nodesArray})
+          })
+        }
+        return res.send(usersArray);
+      })
   );
 
   // Expose Express API as a single Cloud Function:
